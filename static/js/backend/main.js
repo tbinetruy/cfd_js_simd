87011e55c5d9979f4bee0b0a123815e20088734f
0648596375 <tbinetruy@gmail.com>
2016-11-27 18:18:07 +0100

    1d linear advection working

--- static/js/backend/main.js
+++ static/js/backend/main.js
@@ -3,19 +3,38 @@ require('pyextjs')
 onmessage = e => {
 	const { nx, dt, t, c, L } = e.data
 	const dx = L / (nx - 1)
-	const nt = t / dt
+	const nt = t / dt			// number of timesteps to calc
 
 	let u = numpy.ones(nx)
 	const u_0 = u.map( (u_i, i) => {
 		if(i >= Math.floor(0.5 / dx) && i <= Math.floor(1 / dx + 1))
-			return { x: i * dx, y: 2}
+			return 2 
 		else
-			return { x: i * dx, y: 1}
+			return 1
 	})
 
 	u = u_0
+	let u_temp = numpy.ones(nx)
+
+	for(let n = 0; n <= nt; n++) {
+		u_temp = [...u]
+
+		u = u_temp.map((u_i, i) => {
+			if(i > 0)
+				return u_i - c * dt / dx * (u_i - u_temp[i-1]) 
+			else
+				return u_i
+		})
+	}
 
 	postMessage({
-		u_0: u_0,
+		u_0: convertToPlotData(u_0, dx),
+		u: convertToPlotData(u, dx),
+	})
+}
+
+const convertToPlotData = (y, dx) => {
+	return y.map( (y_i, i) => {
+		return { x: i * dx, y: y_i }
 	})
 }
--- static/js/frontend/main.js
+++ static/js/frontend/main.js
@@ -5,7 +5,7 @@ const CFD_worker = new Worker("static/js/backend_bundle.js")
 // start worker
 ;(function() {
 	const config = {
-		nx: 41,				// number of nodes in x dir
+		nx: 61,				// number of nodes in x dir
 		dt: 0.025,			// (s) timestep
 		t: 0.625,			// (s) want solution at time t
 		c: 1,				// convection constant
@@ -20,23 +20,43 @@ const CFD_worker = new Worker("static/js/backend_bundle.js")
 })()
 
 // init chart
-const initChart = function({ u_0 }) {
+const initChart = function({ u_0, u }) {
 	const ctx = document.getElementById("initial-state-chart");
 	const initialStateChart = new Chart(ctx, {
 		type: 'line',
 		data: {
-			datasets: [{
-				label: '# of Votes',
-				data: u_0, 
-				borderWidth: 1
-			}]
+			datasets: [
+				{
+					label: 'u_0',
+					data: u_0, 
+					borderWidth: 1,
+					backgroundColor: 'rgba(0,0,255,0.3)'
+				},
+				{
+					label: 'u',
+					data: u, 
+					borderWidth: 1,
+					backgroundColor: 'rgba(255,0,0,0.3)'
+				},
+			]
 		},
+		xLabels: ['velocity'],
 		options: {
 			responsive: true,
 			scales: {
 				xAxes: [{
 					type: "linear",
 					position: "bottom",
+					scaleLabel: {
+						labelString: "x distance (m)",
+						display: true
+					}
+				}],
+				yAxes: [{
+					scaleLabel: {
+						labelString: "speed (m/s)",
+						display: true
+					}
 				}]
 			}
 		}
