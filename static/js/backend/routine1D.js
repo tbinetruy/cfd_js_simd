e54cfa4fbbe6f2b4e34af28f077ae3963755bc44
0648596375 <tbinetruy@gmail.com>
2016-11-29 00:00:53 +0100

    lquick fix

--- static/js/backend/routine1D.js
+++ static/js/backend/routine1D.js
@@ -1,6 +1,5 @@
 import { PDEs } from "./PDEs/PDEs.js"
-import { solve_FTBS } from "./solvers/FTBS.js"
-import { solve_FTCS } from "./solvers/FTCS.js"
+import { solvers } from "./solvers/solvers.js"
 import { computeIC_1D } from "./IC/IC.js"
 import { compute_param_1D } from "./params/param_1D.js"
 import { solutions } from "./solutions/solutions.js"
@@ -16,19 +15,19 @@ export const routine1D = userInput => {
 	switch(experiment) {
 		case 1:
 			u_0 = computeIC_1D.hat(u, params.dx)
-			u = solve_FTCS._1D(u_0, PDEs.convection.linear._1D, params)
+			u = solvers._1D.explicit.forwardEuler(u_0, PDEs.convection.linear._1D, params)
 			break
 		case 2:
 			u_0 = computeIC_1D.hat(u, params.dx)
-			u = solve_FTCS._1D(u_0, PDEs.convection.nonLinear._1D, params)
+			u = solvers._1D.explicit.forwardEuler(u_0, PDEs.convection.nonLinear._1D, params)
 			break
 		case 3:
 			u_0 = computeIC_1D.hat(u, params.dx)
-			u = solve_FTCS._1D(u_0, PDEs.diffusion._1D, { ...params, c: params.nu })
+			u = solvers._1D.explicit.forwardEuler(u_0, PDEs.diffusion._1D, { ...params, c: params.nu })
 			break
 		case 4:
 			u_0 = computeIC_1D.burgers(u, params)
-			u = solve_FTCS._1D(u_0, PDEs.burgers._1D, { ...params, c: params.nu }, 'periodic')
+			u = solvers._1D.explicit.forwardEuler(u_0, PDEs.burgers._1D, { ...params, c: params.nu }, 'periodic')
 			u_analytical = solutions.burgers._1D(params.nx, params.nt * params.dt, params.nu)
 
 			break
---
+++ static/js/backend/solvers/solvers.js
@@ -0,0 +1,7 @@
+import { explicit } from "./explicit.js"
+
+export const solvers = {
+	_1D: {
+		explicit,
+	}
+}
